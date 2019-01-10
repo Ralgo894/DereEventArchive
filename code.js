06672cfe526421bbf9f380d8a1cf6430e5fe53dd
Ralgo894 <44461594+Ralgo894@users.noreply.github.com>
2019-01-11 02:24:03 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -1,10 +1,23 @@
 var sidePageCount = 2;
 
+var touchStartX, touchStartY, touchMoveX, touchMoveY;
+
 window.addEventListener('resize', changeWindowSize(), false);
 
+window.addEventListener('touchstart', e => {
+  touchStartX = event.touches[0].pageX;
+  touchStartY = event.touches[0].pageY;
+}, false);
+window.addEventListener('touchmove', e => {
+  touchMoveX = event.changedTouches[0].pageX;
+  touchMoveY = event.changedTouches[0].pageY;
+}, false);
+window.addEventListener('touchend', e => {
+
+}, false);
 
 function changeWindowSize() {
-  var slideContainer = document.getElementById('slideContainer');
+  var slideContainer = document.getElementById('pageContainer');
   var pages = document.getElementsByClassName('page');
 
   slideContainer.style.width = window.parent.screen.width * sidePageCount + 'px';
@@ -14,5 +27,4 @@ function changeWindowSize() {
     pages[i].style.height = window.parent.screen.height + 'px';
   }
 }
-
 changeWindowSize();
