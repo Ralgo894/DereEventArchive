b8343d07774c461a2b0c7ae6b7f0c355c970691d
Ralgo894 <44461594+Ralgo894@users.noreply.github.com>
2019-01-11 00:45:21 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -5,9 +5,10 @@ window.addEventListener('resize', changeWindowSize(), false);
 
 function changeWindowSize() {
   var wrapper = document.getElementsByClassName('wrapper')[0];
+  var pages = document.getElementsByClassName('page');
+
   wrapper.style.width = window.parent.screen.width * sidePageCount + 'px';
 
-  var pages = document.getElementsByClassName('page');
   for (var i = 0; pages[i]; i++) {
     pages[i].style.width = window.parent.screen.width + 'px';
     pages[i].style.height = window.parent.screen.height + 'px';
