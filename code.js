a6beb240a04947291262bead0f3824db69e98f43
Ralgo894 <44461594+Ralgo894@users.noreply.github.com>
2019-01-11 00:48:18 +0900

    Update code.js

--- code.js
+++ code.js
@@ -7,11 +7,11 @@ function changeWindowSize() {
   var wrapper = document.getElementsByClassName('wrapper')[0];
   var pages = document.getElementsByClassName('page');
 
-  wrapper.style.width = window.parent.screen.width * sidePageCount + 'px';
+  wrapper.style.width = window.parent.screen.width * sidePageCount * 2 + 'px';
 
   for (var i = 0; pages[i]; i++) {
-    pages[i].style.width = window.parent.screen.width + 'px';
-    pages[i].style.height = window.parent.screen.height + 'px';
+    pages[i].style.width = window.parent.screen.width * 2 + 'px';
+    pages[i].style.height = window.parent.screen.height * 2 + 'px';
   }
 }
 
