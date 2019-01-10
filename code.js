211c99eced7b97971c0b69674422bd0132ba9891
Ralgo894 <44461594+Ralgo894@users.noreply.github.com>
2019-01-11 00:58:33 +0900

    Add files via upload

--- code.js
+++ code.js
@@ -4,14 +4,14 @@ window.addEventListener('resize', changeWindowSize(), false);
 
 
 function changeWindowSize() {
-  var wrapper = document.getElementsByClassName('wrapper')[0];
+  var slideContainer = document.getElementById('slideContainer');
   var pages = document.getElementsByClassName('page');
 
-  wrapper.style.width = window.parent.screen.width * sidePageCount * 2 + 'px';
+  slideContainer.style.width = window.parent.screen.width * sidePageCount + 'px';
 
   for (var i = 0; pages[i]; i++) {
-    pages[i].style.width = window.parent.screen.width * 2 + 'px';
-    pages[i].style.height = window.parent.screen.height * 2 + 'px';
+    pages[i].style.width = window.parent.screen.width + 'px';
+    pages[i].style.height = window.parent.screen.height + 'px';
   }
 }
 
